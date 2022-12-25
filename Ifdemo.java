2ec9ee40ae555835aad9d1e742b1bba40fada1a7
kanniyappan143 <119713241+kanniyappan143@users.noreply.github.com>
2022-12-25 20:42:45 +0530

    Add files via upload

---
+++ Ifdemo.java
@@ -0,0 +1,42 @@
+package controlstatement;
+/*if statement 
+ * syntax
+ * 
+ * if(condition)
+ * {
+ * 		Statement(system.out.println("any"+variable);
+ * }
+ * 
+ * 			Rules of if statement 
+ * if statement is condition was true then execute the inside of the Statement
+ * But if condition false execute the outside statement(example:(System.out.println("value is false"); this is executed in statement is false 
+ *  
+ * }*/
+
+public class Ifdemo 
+{
+	public static void main(String[] args) 
+		{
+			int a=6;
+			int b=4;
+			if(a<7)
+			if(a==6)
+			{
+				System.out.println("value is "+a);
+			}
+		
+		
+	//value is false example 
+	
+		
+		if(b>5)
+		{
+			System.out.println("value is true "+b); 
+		}
+		System.out.println("value is false ");
+		}
+	}
+
+	
+
+
